a93208cd9d755bc622b379ab2aa65f545a77af1c
Nishino0719 <nishino0719@icloud.com>
2025-01-11 19:45:04 +0900

    Add notification utility for sending messages via webhook

---
+++ functions/utils/notification.js
@@ -0,0 +1,13 @@
+export const notification = async (message) => {
+  const WEBHOOK_URL = process.env.WEBHOOK_URL
+
+  await fetch(WEBHOOK_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: 'いつも頑張っていてえらい',
+    }),
+  })
+}
