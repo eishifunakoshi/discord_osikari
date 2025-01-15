93cfff008117605195820e8cbfcae9e2006d9f25
eishifunakoshi <eishi.funakoshi@youlib.co.jp>
2025-01-15 17:17:20 +0900

    インポート修正

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,19 @@
 import { getHighExpenses } from "./utils/freeeApi.js";
 import { notification } from "./utils/notification.js";
-import * as functions from "firebase-functions";
+import { onSchedule } from "firebase-functions/v2/scheduler";
 
-export const weeklyMessage = functions.pubsub
-  .schedule("every 7 days")
-  .onRun(async () => {
-    const expenses = await getHighExpenses(new Date());
+export const weeklyMessage = onSchedule("every 7 days", async () => {
+  const expenses = await getHighExpenses(new Date());
 
-    if (expenses.length === 0) {
-      await notification("やるやん、その調子で励むが良い");
-      return null;
-    } else {
-      for (const expense of expenses) {
-        const message = `📢 **高額交際費の通知**\n- 金額: ¥${expense.amount}\n- 日付: ${expense.date}`;
-        await notification(message);
-      }
-    }
-
+  if (expenses.length === 0) {
+    await notification("やるやん、その調子で励むが良い");
     return null;
-  });
+  } else {
+    for (const expense of expenses) {
+      const message = `📢 **高額交際費の通知**\n- 金額: ¥${expense.amount}\n- 日付: ${expense.date}`;
+      await notification(message);
+    }
+  }
+
+  return null;
+});
