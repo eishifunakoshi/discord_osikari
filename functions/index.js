f215dba02ccf0957f38bc2e4f252fae4d798efe5
eishifunakoshi <eishi.funakoshi@youlib.co.jp>
2025-01-17 11:24:58 +0900

    タイムゾーンの指定

--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,21 @@ import { getHighExpenses } from "./utils/freeeApi.js";
 import { notification } from "./utils/notification.js";
 import { onSchedule } from "firebase-functions/v2/scheduler";
 
-export const weeklyMessage = onSchedule("every 7 days", async () => {
-  const expenses = await getHighExpenses(new Date());
+export const weeklyMessage = onSchedule(
+  { schedule: "every 7 days", timeZone: "Asia/Tokyo" },
+  async () => {
+    const expenses = await getHighExpenses(new Date());
 
-  if (expenses.length === 0) {
-    await notification("やるじゃん、その調子で励むが良い");
-    return null;
-  } else {
-    for (const expense of expenses) {
-      const message = `📢 **高額交際費の通知**\n- 金額: ¥${expense.amount}\n- 日付: ${expense.date}`;
-      await notification(message);
+    if (expenses.length === 0) {
+      await notification("やるじゃん、その調子で励むが良い");
+      return null;
+    } else {
+      for (const expense of expenses) {
+        const message = `📢 **高額交際費の通知**\n- 金額: ¥${expense.amount}\n- 日付: ${expense.date}`;
+        await notification(message);
+      }
     }
-  }
 
-  return null;
-});
+    return null;
+  }
+);
